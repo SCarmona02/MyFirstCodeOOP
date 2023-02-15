610ec33c2d141da37e84f3a6f19d59118682a19f
SCarmona02 <santiago.car2003@hotmail.com>
2023-02-14 19:22:42 -0500

    I created my first code with a new Date Class

---
+++ MyFirstCodeOPP/MyFirstCodeOPP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstCodeOPP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dateObject = new Date(99, 2, 12);
+                Console.WriteLine(dateObject);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
