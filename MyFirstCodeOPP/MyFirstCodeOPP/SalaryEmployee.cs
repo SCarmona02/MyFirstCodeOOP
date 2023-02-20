719462f8f0bc9cafde97473a131a3f9049a6e0df
SCarmona02 <santiago.car2003@hotmail.com>
2023-02-19 23:50:23 -0500

    Creating new concret class called SalaryEmployee

---
+++ MyFirstCodeOPP/MyFirstCodeOPP/SalaryEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOPP
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValuesToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} \n\t " +
+                $"Value to Pay: {GetValuesToPay()}";
+        }
+
+        #endregion
+    }
+}
