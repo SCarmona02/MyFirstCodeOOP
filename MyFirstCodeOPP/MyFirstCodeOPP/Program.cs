62a28467867e8d9f0e4d45278e8ee87ca898fca3
SCarmona02 <santiago.car2003@hotmail.com>
2023-02-20 00:06:40 -0500

    Implementation in Program.cs to show the SalaryEmploye object

--- MyFirstCodeOPP/MyFirstCodeOPP/Program.cs
+++ MyFirstCodeOPP/MyFirstCodeOPP/Program.cs
@@ -8,17 +8,32 @@ namespace MyFirstCodeOPP
         {
             try
             {
-                Console.WriteLine("Write a year");
-                string year = Console.ReadLine();
+                //Console.WriteLine("Write a year");
+                //string year = Console.ReadLine();
 
-                Console.WriteLine("Write a month");
-                string month = Console.ReadLine();
+                //Console.WriteLine("Write a month");
+                //string month = Console.ReadLine();
 
-                Console.WriteLine("Write a day");
-                string day = Console.ReadLine();
+                //Console.WriteLine("Write a day");
+                //string day = Console.ReadLine();
 
-                var dateObject = new Date(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
-                Console.WriteLine(dateObject);
+                //var dateObject = new Date(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
+                //Console.WriteLine(dateObject);
+
+                Console.WriteLine("*******Testing lastes implementation***************");
+
+                Employee salaryEmployee = new SalaryEmployee()
+                {
+                    Id = 1,
+                    FirstName = "Maria",
+                    LastName = "Posada",
+                    BirthDate = new Date(1950,2,5),
+                    HiringDate = new Date(2022,12,31),
+                    IsActive = true,
+                    Salary = 200000.34M
+                };
+
+                Console.WriteLine(salaryEmployee);
 
             }
             catch (Exception ex)
