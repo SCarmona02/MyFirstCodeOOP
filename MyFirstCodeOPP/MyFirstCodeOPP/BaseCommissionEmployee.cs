37ba6516acd2162f1fcba575daee539238ce9548
SCarmona02 <santiago.car2003@hotmail.com>
2023-02-21 16:18:50 -0500

    New BaseCommissionEmployee class created with new inheritance linked to CommissionEmployee class

---
+++ MyFirstCodeOPP/MyFirstCodeOPP/BaseCommissionEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOPP
+{
+    public class BaseCommissionEmployee : CommissionEmployee
+    {
+        #region Properties
+        public decimal Base { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValuesToPay()
+        {
+            //return ((Sales * (decimal)ConvertPercentage(CommissionPercentage)) + Base);
+            return base.GetValuesToPay() + Base;
+        }
+
+        public override string ToString() 
+        {
+            return $"{base.ToString()} \n\t " +
+                $"Salary Base:..............................{Base:C2} \n\t " +
+                $"Value to pay with base....................{GetValuesToPay():C2}";
+        }
+        #endregion
+    }
+}
--- MyFirstCodeOPP/MyFirstCodeOPP/Program.cs
+++ MyFirstCodeOPP/MyFirstCodeOPP/Program.cs
@@ -69,73 +69,73 @@ namespace MyFirstCodeOPP
 
                 Console.WriteLine(salaryEmployee);
 
-                //Console.WriteLine("*******************************");
-                //Console.WriteLine("******* COMMISSION EMPLOYEE *******");
-                //Console.WriteLine("*******************************");
+                Console.WriteLine("*******************************");
+                Console.WriteLine("***** COMMISSION EMPLOYEE *****");
+                Console.WriteLine("*******************************");
 
-                //Console.WriteLine("Write an Id");
-                //id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write an Id");
+                id = Convert.ToInt32(Console.ReadLine());
 
-                //Console.WriteLine("Write a First Name");
-                //firstName = Console.ReadLine();
+                Console.WriteLine("Write a First Name");
+                firstName = Console.ReadLine();
 
-                //Console.WriteLine("Write a Last Name");
-                //lastName = Console.ReadLine();
+                Console.WriteLine("Write a Last Name");
+                lastName = Console.ReadLine();
 
-                //Console.WriteLine("Insert Birth Date");
+                Console.WriteLine("Insert Birth Date");
 
-                //Console.WriteLine("Write a year");
-                //yearBirth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write a year");
+                yearBirth = Convert.ToInt32(Console.ReadLine());
 
-                //Console.WriteLine("Write a month");
-                //monthBirth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write a month");
+                monthBirth = Convert.ToInt32(Console.ReadLine());
 
-                //Console.WriteLine("Write a day");
-                //dayBirth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write a day");
+                dayBirth = Convert.ToInt32(Console.ReadLine());
 
-                //Console.WriteLine("Birth date: " + new Date(yearBirth, monthBirth, dayBirth));
+                Console.WriteLine("Birth date: " + new Date(yearBirth, monthBirth, dayBirth));
 
-                //Console.WriteLine("Insert Hiring Date");
+                Console.WriteLine("Insert Hiring Date");
 
-                //Console.WriteLine("Write a year");
-                //yearHiring = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write a year");
+                yearHiring = Convert.ToInt32(Console.ReadLine());
 
-                //Console.WriteLine("Write a month");
-                //monthHiring = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write a month");
+                monthHiring = Convert.ToInt32(Console.ReadLine());
 
-                //Console.WriteLine("Write a day");
-                //dayHiring = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write a day");
+                dayHiring = Convert.ToInt32(Console.ReadLine());
 
-                //Console.WriteLine("Hiring date: " + new Date(yearHiring, monthHiring, dayHiring));
+                Console.WriteLine("Hiring date: " + new Date(yearHiring, monthHiring, dayHiring));
 
-                //Console.WriteLine("Is Active? (True, False)");
-                //isActive = Convert.ToBoolean(Console.ReadLine());
+                Console.WriteLine("Is Active? (True, False)");
+                isActive = Convert.ToBoolean(Console.ReadLine());
 
-                //Console.WriteLine("Write a commission percentage");
-                //float commissionPercentage = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Write a commission percentage");
+                float commissionPercentage = Convert.ToSingle(Console.ReadLine());
 
-                //Console.WriteLine("Write a sales");
-                //decimal sales = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Write a sales");
+                decimal sales = Convert.ToDecimal(Console.ReadLine());
 
-                ////var dateObject = new Date(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
-                ////Console.WriteLine(dateObject);
+                //var dateObject = new Date(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
+                //Console.WriteLine(dateObject);
 
-                //Employee commissionEmployee = new CommissionEmployee()
-                //{
-                //    Id = id,
-                //    FirstName = firstName,
-                //    LastName = lastName,
-                //    BirthDate = new Date(yearBirth, monthBirth, dayBirth),
-                //    HiringDate = new Date(yearHiring, monthHiring, dayHiring),
-                //    IsActive = isActive,
-                //    CommissionPercentage = commissionPercentage,
-                //    Sales = sales
-                //};
+                Employee commissionEmployee = new CommissionEmployee()
+                {
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    BirthDate = new Date(yearBirth, monthBirth, dayBirth),
+                    HiringDate = new Date(yearHiring, monthHiring, dayHiring),
+                    IsActive = isActive,
+                    CommissionPercentage = commissionPercentage,
+                    Sales = sales
+                };
 
-                //Console.WriteLine(commissionEmployee);
+                Console.WriteLine(commissionEmployee);
 
                 Console.WriteLine("*******************************");
-                Console.WriteLine("******* CONTRACTOR EMPLOYEE *******");
+                Console.WriteLine("***** CONTRACTOR EMPLOYEE *****");
                 Console.WriteLine("*******************************");
 
                 Console.WriteLine("Write an Id");
@@ -198,6 +198,75 @@ namespace MyFirstCodeOPP
                 };
 
                 Console.WriteLine(contractorEmployee);
+
+                Console.WriteLine("********************************");
+                Console.WriteLine("** BASE & COMMISSION EMPLOYEE **");
+                Console.WriteLine("********************************");
+
+                Console.WriteLine("Write an Id");
+                id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Write a First Name");
+                firstName = Console.ReadLine();
+
+                Console.WriteLine("Write a Last Name");
+                lastName = Console.ReadLine();
+
+                Console.WriteLine("Insert Birth Date");
+
+                Console.WriteLine("Write a year");
+                yearBirth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Write a month");
+                monthBirth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Write a day");
+                dayBirth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Birth date: " + new Date(yearBirth, monthBirth, dayBirth));
+
+                Console.WriteLine("Insert Hiring Date");
+
+                Console.WriteLine("Write a year");
+                yearHiring = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Write a month");
+                monthHiring = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Write a day");
+                dayHiring = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Hiring date: " + new Date(yearHiring, monthHiring, dayHiring));
+
+                Console.WriteLine("Is Active? (True, False)");
+                isActive = Convert.ToBoolean(Console.ReadLine());
+
+                Console.WriteLine("Write a salary base");
+                decimal salaryBase = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Write a commission percentage");
+                commissionPercentage = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("Write a sales");
+                sales = Convert.ToDecimal(Console.ReadLine());
+
+                //var dateObject = new Date(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
+                //Console.WriteLine(dateObject);
+
+                Employee baseCommissionEmployee = new BaseCommissionEmployee()
+                {
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    BirthDate = new Date(yearBirth, monthBirth, dayBirth),
+                    HiringDate = new Date(yearHiring, monthHiring, dayHiring),
+                    IsActive = isActive,
+                    CommissionPercentage = commissionPercentage,
+                    Sales = sales,
+                    Base = salaryBase
+                };
+
+                Console.WriteLine(baseCommissionEmployee);
 
 
             }
